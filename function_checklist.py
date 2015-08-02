94f92233901aacf2aacd81c0d97d0b9a98205135
Marco André Machado <marco.santos.machado@gmail.com>
2015-08-01 21:56:22 -0300

    Coding hard way

---
+++ function_checklist.py
@@ -0,0 +1,13 @@
+print "1. Did you start your function definition with def?\n"
+print "2. Does your function name have only characters and _(underscore) characters?\n"
+print "3. Did you put an open parenthesis ( right after the function name?\n"
+print "4. Did you put your arguments after the parenthesis ( separated by commas?\n"
+print "5. Did you make each argument unique (meaning no duplicated names)?\n"
+print "6. Did you put a close parenthesis and a colon ): after the arguments?\n"
+print "7. Did you indent all lines of code you want in the function four spaces? No more, no less.\n"
+print '8. Did you "end" your function by going back to writing with no indent (dedenting we call it)?\n'
+print '\nWhen you run ("use" or "call") a function, check these things:\n'
+print "1. Did you call/use/run this function by typing its name?\n"
+print "2. Did you put the ( character after the name to run it?\n"
+print "3. Did you put the values you want into the parenthesis separated by commas?\n"
+print "4. Did you end the function call with a ) character?\n"
